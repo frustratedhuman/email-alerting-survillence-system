9813840835f3675be50676269a37ee8faa9b73bd
ritesh <sawantritesh007@gmail.com>
2024-02-08 20:30:35 +0530

    create model which can differentiate passing object in camera using cv2 library

---
+++ main.py
@@ -0,0 +1,29 @@
+import cv2
+import time
+
+video = cv2.VideoCapture(0)
+time.sleep(1)
+
+first_frame = None
+
+while True:
+    check, frame = video.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+
+    if first_frame is None:
+        first_frame = gray_frame_gau
+
+    delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
+
+    thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
+    cv2.imshow("My video", dil_frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord("q"):
+        break
+
+video.release()
+
