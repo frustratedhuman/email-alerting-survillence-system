f3fedeab141e42ab3c083bd8e8831700f5fd8538
ritesh <sawantritesh007@gmail.com>
2024-02-12 19:59:21 +0530

    add a code for rectangular box around moving object after motion detection

--- main.py
+++ main.py
@@ -20,6 +20,15 @@ while True:
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
     cv2.imshow("My video", dil_frame)
 
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+    cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
 
     if key == ord("q"):
